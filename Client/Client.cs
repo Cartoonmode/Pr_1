f4b35e257c7c3dae2a787e01bcfe4419b05c2aaf
chaylord <chaylord@bk.ru>
2020-01-20 21:12:21 +0500

    Добавьте файлы проекта.

---
+++ Client/Client.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        private IPEndPoint IpPoint { get; set; }
+        public bool Work { get; set; }
+        private Socket Socket { get; set; }
+
+        public Client(int port, string address)
+        {
+            IpPoint = new IPEndPoint(IPAddress.Parse(address), port);          
+        }
+
+        public void Start()
+        {
+            Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket.Connect(IpPoint);
+            Console.WriteLine("Соединение установленно.");
+            Work = true;
+
+            Task processingSend = new Task(ProcessingSend);
+            processingSend.Start();
+
+            Task processingReceive = new Task(ProcessingReceive);
+            processingReceive.Start();
+        }
+
+        public void Stop()
+        {
+            Socket.Close();
+            Work = false;
+        }
+
+        private void ProcessingSend()
+        {
+            try
+            {
+                while (Work)
+                {
+                    Socket.Send(BitConverter.GetBytes(new Random().Next(10, 99)));
+                    Console.WriteLine("Запрос отправлен.");
+                    Thread.Sleep(4500);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Work = false;
+            }
+
+        }
+
+        private void ProcessingReceive()
+        {
+            try
+            {
+                while (Work)
+                {
+                    byte[] answer = new byte[4];
+                    Socket.Receive(answer, answer.Length, SocketFlags.None);
+                    if (BitConverter.ToInt32(answer, 0) == -1)
+                    {
+                        Console.WriteLine("Исполнение задачи отменено");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число увеличенное на 100: " + BitConverter.ToInt32(answer, 0));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Work = false;
+            }
+        }
+    }
+}
---
+++ practice_1/User.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace practice_1
+{
+    class User
+    {
+        private Socket Socket { get; set; }
+        private Server Server;
+        private int Id { get; set; }
+        private int Tasks = 0;
+
+        public User(Server Server, Socket Socket, int Id)
+        {
+            this.Socket = Socket;
+            this.Server = Server;
+            this.Id = Id;
+        }
+
+        public void Processing()
+        {
+            try
+            {
+                while (Server.Work)
+                {       
+                    
+                    int number = Reading();
+                    Tasks++;
+                    Task task = new Task(() => Working(number));
+                    task.Start();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("User " + Id +" Error: " + e.Message);
+            }
+
+            Exit();
+        }
+
+        private int Reading()
+        {
+            byte[] receiveBuffer = new byte[4];
+            Socket.Receive(receiveBuffer, receiveBuffer.Length, SocketFlags.None);
+            Console.WriteLine("User " + Id + " получен запрос.");
+            return BitConverter.ToInt32(receiveBuffer,0);
+        }
+
+        private void Working(int number)
+        {
+            Thread.Sleep(7000);
+            if (Tasks>1)
+            {
+                Console.WriteLine("Операция прервана");
+                Answer(BitConverter.GetBytes(-1));
+                Tasks--;
+                return;
+            }
+            Tasks--;
+            Answer(BitConverter.GetBytes(number + 100));
+        }
+
+        private void Answer(byte[] answer)
+        {
+            Socket.Send(answer);
+            Console.WriteLine("User " + Id + " отправлен ответ на запрос.");
+        }
+
+        private void Exit()
+        {
+            Socket.Close();
+        }
+    }
+}
